a29da4907c3b780e6480e826e6a63d5406712d0e
comicway <mises.xiaomi@gmail.com>
2026-06-10 13:21:49 -0400

    hello

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
+// https://vite.dev/config/ hello
 export default defineConfig({
   plugins: [
     react(),
